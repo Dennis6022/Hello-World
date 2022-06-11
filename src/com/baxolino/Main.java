df75e707fdbf62b083cad5673f829be83f4f203d
Dennis6022 <dennis.littawe1542@gmail.com>
2022-06-11 10:43:24 +0200

    first commit

---
+++ src/com/baxolino/Main.java
@@ -0,0 +1,18 @@
+package com.baxolino;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int mein_alter = 22;
+        int year = 2022;
+        System.out.println("Das Jahr ist " + year + "und mein alter " + mein_alter);
+
+        String name = "Dennis";
+        String friend = "Kumaraswamy";
+        System.out.println(name + " mit " + friend);
+        System.out.println();
+
+
+    }
+}
